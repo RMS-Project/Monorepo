d3de8ff81fe2d20b9e1349d17b486250bdf95103
RMS-Project <105136548+RMS-Project@users.noreply.github.com>
2022-09-27 16:52:55 -0300

    Update form

--- packages/web/src/pages/SignIn.js
+++ packages/web/src/pages/SignIn.js
@@ -1,25 +1,75 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom";
 
 import logo from '../logo.svg';
 import '../App.css';
 
 export default function SignIn() {
+  // userState - Hook do react.
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  // Evento sintético que encapsula o evento padrão do JavaScript do browser.
+  // Ele consegue executar alguns eventos padrões.
+  const handleSubmit = (event) => {
+    // Evento padrão.
+    event.preventDefault()
+    fetch('http://loclahost:8080/authenticate', {
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+      .then((response) => response.json())
+      .then(() => {
+        console.log('Success!')
+      })
+  }
+
+  // Ver a biblioteca Form que abstrai a manipulação de formulários quando
+  // ficam mais complexos.
+  const handleEmailChange = event => setEmail(event.target.value)
+  const handlePasswordChange = event => setPassword(event.target.value)
+
+  // onChange - Neste caso serve para controlar melhor o evento.
   return (
     <div className="App-div">
       <img src={logo} className="App-logo" alt="logo" />
       <h1>Sign In</h1>
 
-      <form action="/authenticate" method="POST">
+      <form onSubmit={ handleSubmit }>
       
         <fieldset className="App-fieldset">
-          <label for="email">E-mail</label>
-          <input className="App-input" type="email" name="email" inputmode="email" autocomplete="username"/>
+          <label htmlFor="email">E-mail</label>
+          <input 
+            className="App-input" 
+            id="email"
+            type="email" 
+            inputmode="email" 
+            autocomplete="username"
+
+            onChange={handleEmailChange}
+            value={email}
+          />
         </fieldset>
         
         <fieldset className="App-fieldset">
-          <label for="password">Password</label>
-          <input className="App-input" type="password" name="password" inputmode="password" autocomplete="current-password"/>
+          <label htmlFor="password">Password</label>
+          <input 
+            className="App-input" 
+            id="password"
+            type="password" 
+            inputmode="password"
+            autocomplete="current-password"
+          
+            onChange={handlePasswordChange}
+            value={password}
+
+          />
         </fieldset>
 
         <button className="App-button" type="submit">Entrar</button>
